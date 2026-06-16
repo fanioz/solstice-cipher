46a7acf0188d9edf3fb04a6c98155bf2e1c84ab9
fanioz <fanio.zilla@gmail.com>
2026-06-17 05:59:21 +0800

    feat(level6): add splitter introduction tutorial

--- src/ui/tutorial_ui.gd
+++ src/ui/tutorial_ui.gd
@@ -1,11 +1,12 @@
 extends CanvasLayer
 
-@onready var instruction_1 = $Instruction1
-@onready var instruction_2 = $Instruction2
-@onready var instruction_3 = $Instruction3
+@onready var instruction_1 = get_node_or_null("Instruction1")
+@onready var instruction_2 = get_node_or_null("Instruction2")
+@onready var instruction_3 = get_node_or_null("Instruction3")
 
 func _ready():
-	instruction_3.hide()
+	if instruction_3:
+		instruction_3.hide()
 	for child in get_children():
 		if child is CanvasItem:
 			child.modulate.a = 0.3
@@ -18,8 +19,10 @@ func _ready():
 		drop_zone.item_dropped_on_board.connect(_on_item_dropped)
 
 func _on_item_dropped(tool_type: String, drop_position: Vector2, _slot_ref: Node) -> void:
-	if tool_type == "mirror" and not instruction_3.visible:
+	if instruction_1:
 		instruction_1.hide()
+		
+	if tool_type == "mirror" and instruction_3 and not instruction_3.visible:
 		instruction_3.show()
 		# Position Instruction 3 above the drop position
 		# Center the label (width is 600)
